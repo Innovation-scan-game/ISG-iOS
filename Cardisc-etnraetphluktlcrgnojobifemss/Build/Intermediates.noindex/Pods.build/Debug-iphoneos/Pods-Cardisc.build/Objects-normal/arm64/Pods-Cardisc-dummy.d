a99ba052c77a44035fbd3d0430899ae15e21e9ae
Castaran <timvkesteren@gmail.com>
2023-01-21 23:17:17 +0100

    Rollback to before namechange

--- Cardisc-etnraetphluktlcrgnojobifemss/Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Pods-Cardisc.build/Objects-normal/arm64/Pods-Cardisc-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/tim/Documents/XCode/ISG-iOS1/Pods/Target\ Support\ Files/Pods-Cardisc/Pods-Cardisc-dummy.m \
-  /Users/tim/Documents/XCode/ISG-iOS1/Cardisc-etnraetphluktlcrgnojobifemss/Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Pods-Cardisc.build/module.modulemap
--- Cardisc-etnraetphluktlcrgnojobifemss/Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Pods-Cardisc.build/Objects-normal/arm64/Pods_Cardisc_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/tim/Documents/XCode/ISG-iOS1/Cardisc-etnraetphluktlcrgnojobifemss/Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Pods-Cardisc.build/DerivedSources/Pods_Cardisc_vers.c
