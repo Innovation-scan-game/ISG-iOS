0a6c5fa183126165cc5322d0b01a8a0dcb2b5ce7
Castaran <timvkesteren@gmail.com>
2023-01-24 13:18:57 +0100

    some minimal validation on registering
    password length minimal 5 chars
    emailadress validation

--- Cardisc/Viewmodels/RegisterViewModel.swift
+++ Cardisc/Viewmodels/RegisterViewModel.swift
@@ -22,20 +22,34 @@ class RegisterViewModel: ObservableObject {
     func registerUser() {
         DispatchQueue.main.async {
             if(!self.username.isEmpty && !self.emailaddress.isEmpty && !self.password.isEmpty && !self.passwordRepeated.isEmpty) {
-                if(self.password == self.passwordRepeated) {
-                    self.accountManager.registerUser(username: self.username, password: self.password, emailadress: self.emailaddress) { data in
-                        if (data != nil) {
-                            self.userRegistered = true
-                            self.errorMsg = ""
+                let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+                let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+                if emailTest.evaluate(with: self.emailaddress) {
+                    if(self.password == self.passwordRepeated) {
+                        if(self.password.count > 4) {
+                            self.accountManager.registerUser(username: self.username, password: self.password, emailadress: self.emailaddress) { data in
+                                if (data != nil) {
+                                    self.userRegistered = true
+                                    self.errorMsg = ""
+                                }
+                                else {
+                                    self.errorMsg = "register_error_1"
+                                }
+                            }
                         }
                         else {
-                            self.errorMsg = "Usercredentials incorrect, please try again."
+                            self.errorMsg = "register_error_2"
                         }
+                        
+                    }
+                    else {
+                        self.errorMsg = "Passwords aren't matching"
                     }
                 }
                 else {
-                    self.errorMsg = "Passwords aren't matching"
+                    self.errorMsg = "Emailadres incorrect"
                 }
+                
             }
             else {
                 self.errorMsg = "Some fields are empty"
